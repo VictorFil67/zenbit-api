320bad6a7962a6f79f3ff54b4d3d67918729d007
VictorFil67 <victorfilinovych@gmail.com>
2025-08-29 18:33:30 +0300

    inisiate zenbit-api

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-// import mongoose from "mongoose";
 import dotenv from "dotenv";
-// import authRouter from "./routes/authRouter.js";
-// import picturesRouter from "./routes/picturesRouter.js";
 import { prisma } from "./helpers/prisma.js";
 
 dotenv.config();
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.json("Hello from Zhinomyr!!!");
-// });
-
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
-
-// app.use("/users", authRouter);
-// app.use("/pictures", picturesRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
@@ -31,18 +21,6 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 const { DB_HOST, PORT = 4000 } = process.env;
-// mongoose
-//   .connect(DB_HOST)
-//   .then(() => {
-//     console.log("Database connection successful");
-//     app.listen(PORT, () => {
-//       console.log(`Server is running. Use our API on port: ${PORT}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error.message);
-//     process.exit(1);
-//   });
 
 async function startServer() {
   try {
